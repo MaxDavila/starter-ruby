67955ddc4896a83f9ee00fc4bfa877271258eaa7
maxdavila <adavila0711@gmail.com>
2013-09-22 22:20:13 -0700

    add voicemail api call

--- app.rb
+++ app.rb
@@ -1,6 +1,9 @@
 require 'rubygems'
 require 'sinatra'
 require 'twilio-ruby'
+require 'dotenv'
+
+Dotenv.load 
 
 # Load configuration from system environment variables - see the README for more
 # on these variables.
@@ -55,3 +58,47 @@ get '/hello' do
   content_type 'text/xml'
   response.text
 end
+
+# Render a TwiML document that will say a message back to the user
+get '/hello_sms' do
+  # Build a TwiML response
+  response = Twilio::TwiML::Response.new do |r|
+    r.Sms 'Hello there! You have successfully configured a web hook.'
+    r.Sms 'Good luck on your Twilio quest!', :voice => 'woman'
+  end
+
+  # Render an XML (TwiML) document
+  content_type 'text/xml'
+  response.text
+end
+
+post '/voicemail' do
+  response = Twilio::TwiML::Response.new do |r|
+
+    if params[:Digits]
+      case params[:Digits]
+      when "1"
+        r.Say "Please leave a message at the beep"
+        r.Say "Press the pound key when finished"
+        r.Record(action: '/voicemail', method: 'POST', maxLength: 20, finishOnKey: '#')
+      when "2"
+        client.account.recordings.list.take(3).each do |recording|
+          r.Play recording.wav
+        end
+      when "#"
+        r.Say "Goodbye"
+      else
+        r.Say "Invalid input"
+      end
+    else
+      r.Gather numDigits: 1 do
+        r.Say "Thanks for calling Whoop Whoop where "
+        r.Say "Press 1 to leave a message"
+        r.Say "Press 2 to listen to the last message"
+      end
+    end
+  end
+  content_type 'text/xml'
+  response.text
+end
+
